4d278cfa7444ae304a28cd98de44196b102868e6
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2013-09-03 15:41:19 +0200

    minor clean-ups

--- docbench.py
+++ docbench.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 """
-Docbench -- Benchmarking with Doctest
-"""
-
-"""
-(benchmarks are no good when the tests don't pass ... and DRY !)
-The typical way to use this module would be to have the usal doctest base
-(internal or external, as you wish) that covers a lot but for which every
-test is fast ... AND then another "object" (file, function , etc. probably
-external) of long-running tests that are meant too be benchmark but can
-be tested too and use to generate profiles.
+Docbench -- benchmarks based on doctests
 """
 
 # Python 2.7 Standard Library
@@ -18,31 +9,14 @@ import argparse
 import cProfile
 import doctest
 import json
+import gc
 import os.path
 import time
 import sys
 
 # Third-Party Libraries
-import path # forked-path
+import path # from "path.py"
 import lsprofcalltree
-
-#
-# Misc. Notes
-# ==============================================================================
-#
-#   - produce a benchmark table that may be used to compare performance between
-#     several versions ? (see benchrun on Google Code).
-#
-#   - the idea is to investigate doctest and use them for the three purposes.
-#     For every test, there is potentialy a setup. For a doctest, if there are
-#     multiple statements, all but the last one would be considered setup.
-#
-#   - The timeit module is short, pure Python code with a ugly interface. 
-#     Don't rely on it. See how DoctTestRunner __run method is implemented
-#     and instrument that.
-#
-#   - Is "bench the last statement only" the good idea ?
-#
 
 #
 # Metadata
@@ -54,28 +28,8 @@ __url__ = None
 __version__ = None
 
 #
-# Misc. Notes
 # ------------------------------------------------------------------------------
 #
-#
-# TODO: support object (function, module, etc.) additionally to test.
-#       Support lists of stuff ? Rather multiple positional arguments ?
-#       What about the fact that we want all objects to be named ? Support
-#       keyword arguments for them ? Could be nice but will be unsorted
-#       then ... In a first approach, we may assume that everything is
-#       properly named.
-#
-# TODO: extract short description from the test if any ? (if it does not
-#       start with ">>>" ...)
-#
-# TODO: introduce timeit number and repeat ? (and post-process after repeat ?)
-#       consider disabling gc ?
-#
-# TODO: implement a "-m" mode like doctest.
-#
-# TODO: for sequence of benchs in a module, can we ensure that source order is 
-#       preserved ?
-# 
 
 def _source_order(self, other):
     """
@@ -115,18 +69,15 @@ def benchmark(*objects, **options):
             for code in codes[:-1]: # setup 
                 exec code in globs
             statement = codes[-1]
+            gc.disable()
             start = time.time()
             exec statement in globs
             stop = time.time()
+            gc.enable()
+            gc.collect()
             times.append(stop - start)
         results.append((test.name, filter(times)))
     return results
-
-# OK, profile groks code object too ... with runctx we should be able to do
-# what we want to do.
-
-# profile options for kcg (save to file or let the user do it) ? use a tmp
-# dir and LAUNCH kcg ? Support pstats stuff ?
 
 def profile(output, *objects):
     tests = get_tests(*objects)
@@ -147,61 +98,6 @@ def profile(output, *objects):
         kcg_profile = lsprofcalltree.KCacheGrind(profile)
         kcg_profile.output(output_file)
         output_file.close()
-
-"""
-def profile(command, output=None):
-    output = "bitstream.kcg"
-    output_file = open(output, "w")   
-    profile = cProfile.Profile()
-    profile.run(command)
-    kcg_profile = lsprofcalltree.KCacheGrind(profile)
-    kcg_profile.output(output_file)
-    output_file.close()   
-"""
-
-
-#
-# DocTests 
-# ------------------------------------------------------------------------------
-#
-
-def meh():
-    """
-    >>> z = 7
-    """
-
-class C(object):
-    """
-    >>> c = 1
-    """
-    def __init__(self):
-        """
-        >>> z = 8
-        """
-        pass
-
-def test():
-   """
-   >>> a = 1
-   >>> a
-   1
-   >>> a + 1
-   2
-   >>> 2 * a
-   2
-   >>> while a < 10000000:
-   ...     a += 1
-   >>> fib()
-   42
-   """
-   pass
-
-def fib():
-    a, b = 1, 1
-    while a < 100000000000:
-        a, b = b, a + b
-    return 42
-    
 
 #
 # Table Formatter
@@ -265,10 +161,17 @@ def main(**kwargs):
 def testargs(**kwargs):
     print kwargs
 
+
+#
+# Command-Line Interface
+# ------------------------------------------------------------------------------
+#
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run benchmarks")
     parser.add_argument("filename", metavar="FILENAME", help="benchmark container")
-    parser.add_argument("-o", "--output", nargs="?", type=argparse.FileType('w'), default=sys.stdout,
+    parser.add_argument("-o", "--output", nargs="?", 
+                        type=argparse.FileType("w"), default=sys.stdout,
                         help="output file")
     parser.add_argument("-f", "--format", nargs="?", default="text")
     parser.add_argument("-p", "--profile", action="store_true", default=False)
