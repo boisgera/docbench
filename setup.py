7c66f218090e3bd121e60a03f9f777d2555880f4
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2013-09-03 15:38:00 +0200

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,59 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import distutils.core
+# Python 2.7 Standard Library
 import os.path
+import re
 import sys
+
+# Third-Party Libraries
+import setuptools
+
 
 
 def get_info(module):
-    "Extract distutils metadata from a module"
+    "Extract module metadata"
  
     info = {}
 
-    name = getattr(module, "__name__", None)
-    if not name:
-        name = module.__file__.split(".")[0]
-    info["name"] = name
+    # Read the relevant __*__ module attributes
+    for name in "name author license url version doc".split():
+        value = getattr(module, "__" + name + "__", None)
+        if value:
+            info[name] = value
 
-    author_info = getattr(module, "__author__", None)
-    if author_info:
-        # "author <author_email>" syntax expected
-        info["author"] = author_info.split("<")[0].strip()
-        info["author_email"] = author_info.split("<")[1].strip()[:-1]
+    # Search for author email with <...@...> syntax in the author field
+    author = info.get("author", None)
+    if author:
+        email_pattern = r"<([^>]+@[^>]+)>"
+        match = re.search(email_pattern, author)
+        if match:
+            info["author_email"] = author_email = match.groups()[0]
+            info["author"] = author.replace("<" + author_email + ">", "").strip()
 
-    license = getattr(module, "__license__", None)
-    if license:
-        info["license"] = license
-
-    url = getattr(module, "__url__", None)
-    if url:
-        info["url"] = url
-
-    version = getattr(module, "__version__", None)
-    if version:
-        info["version"] = version
-
-    doc = getattr(module, "__doc__", None)
+    # Get the module synopsis (first non-blank line) from its docstring
+    doc = info.get("doc", None)
     if doc:
-        # the description is the first non-blank doc line
         lines = doc.splitlines()
         for line in lines:
             if line:
-                info["description"] = line
+                info["description"] = line.strip()
                 break
-
-    module = getattr(module, "__file__")
-    if module:
-        info["py_modules"] = [os.path.basename(module).split(".")[0]]
+    del info["doc"]
 
     return info
 
+sys.path.insert(0, ".")
+import docbench
+info = get_info(docbench)
+
+extra = dict(
+  py_modules       = [info["name"]],
+  scripts          = ["scripts/docbench"],
+  install_requires = ["path.py"],
+)
+
+info.update(extra)
+
+
+print info
 
 if __name__ == "__main__":
-    sys.path.insert(0, ".")
-    import docbench
-    info = get_info(docbench)
-    info["scripts"] = ["docbench"]
-    distutils.core.setup(**info)
+    setuptools.setup(**info)
 
