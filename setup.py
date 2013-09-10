f2f08e528fecf4323b74cbd70da42dba421fe521
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2013-09-10 16:16:43 +0200

    improved setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 # Python 2.7 Standard Library
+import inspect
+import os
 import os.path
 import re
 import sys
@@ -9,55 +11,84 @@ import sys
 # Third-Party Libraries
 import setuptools
 
+# ------------------------------------------------------------------------------
 
-
-def get_info(module):
-    "Extract module metadata"
+def get_metadata(module):
+    "Extract and process embedded module metadata"
  
-    info = {}
+    metadata = {}
 
     # Read the relevant __*__ module attributes
-    for name in "name author license url version doc".split():
+    for name in "name author license url classifiers".split():
         value = getattr(module, "__" + name + "__", None)
         if value:
-            info[name] = value
+            metadata[name] = value
 
     # Search for author email with <...@...> syntax in the author field
-    author = info.get("author", None)
+    author = metadata.get("author", None)
     if author:
         email_pattern = r"<([^>]+@[^>]+)>"
         match = re.search(email_pattern, author)
         if match:
-            info["author_email"] = author_email = match.groups()[0]
-            info["author"] = author.replace("<" + author_email + ">", "").strip()
+            metadata["author_email"] = email = match.groups()[0]
+            metadata["author"] = author.replace("<" + email + ">", "").strip()
 
-    # Get the module synopsis (first non-blank line) from its docstring
-    doc = info.get("doc", None)
+    # Get the module short/long description from the docstring
+    doc = inspect.getdoc(module) or ""
     if doc:
         lines = doc.splitlines()
-        for line in lines:
-            if line:
-                info["description"] = line.strip()
-                break
-    del info["doc"]
+        metadata["description"] = lines[0]
+        extra = "\n".join(lines[1:]).strip()
+        if extra:
+            metadata["long_description"] = extra
 
-    return info
+    # Process trove classifiers
+    classifiers = metadata.get("classifiers", None)
+    if classifiers and isinstance(classifiers, str):
+        classifiers = [l.strip() for l in classifiers.splitlines() if l.strip()]
+        metadata["classifiers"] = classifiers
+
+    return metadata
+
+def get_some_reST(markdown):
+    "Markdown to reStructuredText converter"
+    try:
+        import sh; pandoc = sh.pandoc
+    except:
+        error  = "sh.py and/or pandoc not available.\n"
+        error += "  sh.py:  <http://amoffat.github.io/sh/>\n"
+        error += "  pandoc: <http://johnmacfarlane.net/pandoc/>"
+        raise ImportError(error)
+    with open("tmp.md", "w") as markdown_file:
+        markdown_file.write(markdown)
+    restructuredtext = sh.pandoc("-w", "rst", "tmp.md")
+    os.remove("tmp.md")
+    return restructuredtext
+
+
+# ------------------------------------------------------------------------------
 
 sys.path.insert(0, ".")
 import docbench
-info = get_info(docbench)
+metadata = get_metadata(docbench)
 
-extra = dict(
-  py_modules       = [info["name"]],
-  scripts          = ["scripts/docbench"],
-  install_requires = ["path.py"],
+contents = dict(
+  py_modules = [metadata["name"]],
 )
 
-info.update(extra)
+requirements = dict(
+  install_requires = ["lsprofcalltree", "path.py==3.2"],
+)
 
-
-print info
+info = {}
+info.update(metadata)
+info.update(contents)
+info.update(requirements)
 
 if __name__ == "__main__":
+    if "register" in sys.argv[1:]:
+         long_description = metadata["long_description"]
+         if long_description:
+              metadata["long_description"] = get_some_reST(long_description)
     setuptools.setup(**info)
 
