f860aee66b8bebc80833328bc26e66e8933451bd
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2018-03-12 18:08:21 +0100

    vendor libs tweaks

--- setup.py
+++ setup.py
@@ -1,29 +1,54 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Python 2.7 Standard Library
+# Python Standard Library
 import inspect
-import os
+import os.path
 import re
 import sys
 
-# Third-Party Libraries
-import setuptools
+# Pip Package Manager
+try:
+    import pip
+    import setuptools
+    import pkg_resources
+except ImportError:
+    error = "pip is not installed, refer to <{url}> for instructions."
+    raise ImportError(error.format(url="http://pip.readthedocs.org"))
 
-# Local Libraries
-sys.path.insert(0, "lib"); import about
-sys.path.insert(0, "docbench"); import __about__ as about_docbench
+# Redistributed Third-Party ("Vendor") Libraries 
+def local(path):
+    return os.path.join(os.path.dirname(__file__), path)
 
-lines = open("README.md").read().splitlines()
-about_docbench.__doc__ = "\n".join([lines[0]] + lines[2:])
-metadata = about.get_metadata(about_docbench)
+sys.path.insert(1, local(".lib"))
+try:
+    setup_requires = ["about>=5.2<6"]
+    require = lambda *r: pkg_resources.WorkingSet().require(*r)
+    require(*setup_requires)
+    import about
+except pkg_resources.DistributionNotFound:
+    error = """{req!r} not found; install it locally with:
+
+    pip install --target=.lib --ignore-installed {req!r}
+"""
+    raise ImportError(error.format(req=" ".join(setup_requires)))
+
+
+# Setup
+# ------------------------------------------------------------------------------
+sys.path.insert(0, "docbench"); import __about__
+metadata = about.get_metadata(__about__)
 
 contents = dict(
   packages = ["docbench"],
 )
 
+install_requires = ["lsprofcalltree"]
+if sys.version_info < (3, 0):
+    install_requires.append("pathlib2")
+
 requirements = dict(
-  install_requires = ["lsprofcalltree", "pathlib2"],
+  install_requires = install_requires,
 )
 
 info = {}
